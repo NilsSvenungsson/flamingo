1811e638788b2ca240cb627bd4b7c38684840802
Nils Svenungsson <113343467+NilsSvenungsson@users.noreply.github.com>
2024-02-22 11:14:03 +0100

    alla filer

---
+++ js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.flamingo = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(Drupal);
